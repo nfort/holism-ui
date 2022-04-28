e96ff0f1373e375227d1e15df6e2204e5a425a6b
Малякин Кирилл <kirill.malyakin@gazprombank.ru>
2022-04-28 14:56:21 +0300

    fix: добавил экспорт ModalWindow

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 export { default as Slider } from "./Slider"
 export { FieldLoading } from "./FieldLoading/FieldLoading";
+export { default as ModalWindow } from './ModalWindow/';
+export { default as ModalRoot } from './ModalWindow/ModalRoot';
+export { default as ModalHeader } from './ModalWindow/Header';
+export { default as ModalFooter } from './ModalWindow/Footer';
+export { default as ModalContent } from './ModalWindow/Content';
+export { default as useModal } from './ModalWindow/utils/useModal';
