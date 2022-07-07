e44ecead4c088b4b7ebdee4298553ca009760cfc
Kirill Malyakin <kirillmalyakin@MacBook-Air-Kirill.local>
2022-07-07 14:39:52 +0300

    feat: добавил экспорт Input, TextArea, Tooltip

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-export { default as Slider } from "./Slider"
+export { default as Slider } from "./Slider";
 export { FieldLoading } from "./FieldLoading/FieldLoading";
-export { default as ModalWindow } from './ModalWindow/';
-export { default as ModalRoot } from './ModalWindow/ModalRoot';
-export { default as ModalHeader } from './ModalWindow/Header';
-export { default as ModalFooter } from './ModalWindow/Footer';
-export { default as ModalContent } from './ModalWindow/Content';
-export { default as useModal } from './ModalWindow/utils/useModal';
+export { default as ModalWindow } from "./ModalWindow/";
+export { default as ModalRoot } from "./ModalWindow/ModalRoot";
+export { default as ModalHeader } from "./ModalWindow/Header";
+export { default as ModalFooter } from "./ModalWindow/Footer";
+export { default as ModalContent } from "./ModalWindow/Content";
+export { default as useModal } from "./ModalWindow/utils/useModal";
+export { default as Input } from "./Input";
+export { default as TextArea } from "./TextArea";
+export { default as Tooltip } from "./Tooltip/Tooltip";
